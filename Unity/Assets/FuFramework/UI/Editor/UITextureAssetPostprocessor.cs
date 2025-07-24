9cfdac0a69d4ad46c441ef167758e276fa26e1bf
Zhuzhifu <Zhuzhifu@qq.com>
2025-07-24 11:34:33 +0800

    fix：UI纹理导入设置修改

--- Unity/Assets/FuFramework/UI/Editor/UITextureAssetPostprocessor.cs
+++ Unity/Assets/FuFramework/UI/Editor/UITextureAssetPostprocessor.cs
@@ -10,7 +10,9 @@ namespace FuFramework.UI.Editor
     {
         private void OnPreprocessTexture()
         {
-            if (!assetPath.Contains(PathHelper.Combine(Utility.Asset.Path.BundlesPath, "UI"))) return;
+            var isBundleUI = assetPath.Contains(PathHelper.Combine(Utility.Asset.Path.BundlesPath, "UI"));
+            var isResourceUI = assetPath.Contains(PathHelper.Combine("Resources", "UI"));
+            if (!isBundleUI && !isResourceUI) return;
             
             var textureImporter = (TextureImporter)assetImporter;
            
@@ -19,6 +21,9 @@ namespace FuFramework.UI.Editor
 
             if (textureImporter.mipmapEnabled)
                 textureImporter.mipmapEnabled = false;
+            
+            if (textureImporter.isReadable)
+                textureImporter.isReadable = false;
 
             textureImporter.alphaSource         = TextureImporterAlphaSource.FromInput;
             textureImporter.alphaIsTransparency = true;
