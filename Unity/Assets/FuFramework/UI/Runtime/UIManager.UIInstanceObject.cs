4aac0b6100b98b7a99ec280cd404baa96b5aa49b
Zhuzhifu <Zhuzhifu@qq.com>
2025-07-25 18:45:40 +0800

    fix：优化UI框架

--- Unity/Assets/FuFramework/UI/Runtime/UIManager.UIInstanceObject.cs
+++ Unity/Assets/FuFramework/UI/Runtime/UIManager.UIInstanceObject.cs
@@ -33,6 +33,7 @@ namespace FuFramework.UI.Runtime
                 if (Target is not ViewBase viewBase)
                     throw new GameFrameworkException("[UIInstanceObject]需要释放的目标对象不是界面基类ViewBase");
                 
+                viewBase.UIView?.Dispose();
                 viewBase._OnDispose();
             }
         }
