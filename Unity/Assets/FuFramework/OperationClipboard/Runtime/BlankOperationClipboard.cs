77e63bbc2e80dea06c363e8e7eedcb27fea0d736
Zhuzhifu <Zhuzhifu@qq.com>
2025-08-18 18:48:59 +0800

    fix：整理规范框架命名空间-BlankOperationClipboard粘贴板工具

--- Unity/Assets/FuFramework/OperationClipboard/Runtime/BlankOperationClipboard.cs
+++ Unity/Assets/FuFramework/OperationClipboard/Runtime/BlankOperationClipboard.cs
@@ -3,11 +3,13 @@
 using System.Runtime.InteropServices;
 #endif
 
-/// <summary>
-/// 粘贴板的读写
-/// </summary>
-public static class BlankOperationClipboard
+namespace FuFramework.OperationClipboard.Runtime
 {
+    /// <summary>
+    /// 粘贴板的读写
+    /// </summary>
+    public static class BlankOperationClipboard
+    {
 #if UNITY_IOS
 	[DllImport("__Internal")]
 	private static extern string GetClipBoard ();
@@ -17,12 +19,12 @@ public static class BlankOperationClipboard
 #endif
 
 
-    /// <summary>
-    /// 获取粘贴板的值
-    /// </summary>
-    /// <returns></returns>
-    public static string GetValue()
-    {
+        /// <summary>
+        /// 获取粘贴板的值
+        /// </summary>
+        /// <returns></returns>
+        public static string GetValue()
+        {
 #if UNITY_ANDROID
         using (AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.alianhome.operationclipboard.MainActivity"))
         {
@@ -31,30 +33,30 @@ public static class BlankOperationClipboard
 #elif UNITY_IOS
 	    return GetClipBoard ();
 #else
-        return UnityEngine.GUIUtility.systemCopyBuffer ?? string.Empty;
+            return GUIUtility.systemCopyBuffer ?? string.Empty;
 #endif
-    }
+        }
 
-    /// <summary>
-    /// 设置粘贴板的值
-    /// </summary>
-    /// <param name="text"></param>
-    public static void SetValue(string text)
-    {
+        /// <summary>
+        /// 设置粘贴板的值
+        /// </summary>
+        /// <param name="text"></param>
+        public static void SetValue(string text)
+        {
 #if UNITY_EDITOR || UNITY_STANDALONE
-        TextEditor textEditor = new TextEditor();
-        textEditor.text = text;
-        textEditor.OnFocus();
-        textEditor.Copy();
+            var textEditor = new TextEditor { text = text };
+            textEditor.OnFocus();
+            textEditor.Copy();
 #elif UNITY_ANDROID
         using (AndroidJavaClass androidJavaClass = new AndroidJavaClass("com.alianhome.operationclipboard.MainActivity"))
         {
              androidJavaClass.CallStatic("SetClipBoard", text);
         }
 #elif UNITY_IOS
-		SetClipBoard (text);
+	SetClipBoard (text);
 #else
-        UnityEngine.GUIUtility.systemCopyBuffer = text;
+        GUIUtility.systemCopyBuffer = text;
 #endif
+        }
     }
 }
