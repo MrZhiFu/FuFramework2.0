4d82645834a5b60a446e4352f44707082f4718a9
Zhuzhifu <Zhuzhifu@qq.com>
2025-09-18 18:38:49 +0800

    fix：框架精简删掉废弃代码

--- Unity/Assets/FuFramework/Core/Runtime/Base/Log/ELogLevel.cs
+++ Unity/Assets/FuFramework/Core/Runtime/Base/Log/ELogLevel.cs
@@ -4,7 +4,7 @@ namespace FuFramework.Core.Runtime
     /// <summary>
     /// 游戏框架日志等级。
     /// </summary>
-    public enum LogLevel : byte
+    public enum ELogLevel : byte
     {
         /// <summary>
         /// 调试。
--- Unity/Assets/FuFramework/Core/Runtime/Base/Log/FuLog.cs
+++ Unity/Assets/FuFramework/Core/Runtime/Base/Log/FuLog.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 // ReSharper disable once CheckNamespace
 namespace FuFramework.Core.Runtime
@@ -6,49 +7,50 @@ namespace FuFramework.Core.Runtime
     /// <summary>
     /// 游戏框架日志类。
     /// </summary>
-    public static partial class FuLog
+    public static class FuLog
     {
         /// <summary>
-        ///     打印调试级别日志，用于记录调试类日志信息。
+        /// 打印调试级别日志，用于记录调试类日志信息。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
         [Conditional("ENABLE_DEBUG_LOG")]
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
-        public static void Debug(object message) => LogHelper.Log(LogLevel.Debug, message);
+        public static void Debug(object message) => Log(ELogLevel.Debug, message);
 
         /// <summary>
-        ///     打印调试级别日志，用于记录调试类日志信息。
+        /// 打印调试级别日志，用于记录调试类日志信息。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
         [Conditional("ENABLE_DEBUG_LOG")]
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
-        public static void Debug(string message) => LogHelper.Log(LogLevel.Debug, message);
+        public static void Debug(string message) => Log(ELogLevel.Debug, message);
 
+        
         /// <summary>
-        ///     打印信息级别日志，用于记录程序正常运行日志信息。
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
         [Conditional("ENABLE_INFO_LOG")]
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
-        public static void Info(object message) => LogHelper.Log(LogLevel.Info, message);
+        public static void Info(object message) => Log(ELogLevel.Info, message);
 
         /// <summary>
-        ///     打印信息级别日志，用于记录程序正常运行日志信息。
+        /// 打印信息级别日志，用于记录程序正常运行日志信息。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
         [Conditional("ENABLE_INFO_LOG")]
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
-        public static void Info(string message) => LogHelper.Log(LogLevel.Info, message);
+        public static void Info(string message) => Log(ELogLevel.Info, message);
 
 
         /// <summary>
-        ///     打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -56,10 +58,10 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
-        public static void Warning(object message) => LogHelper.Log(LogLevel.Warning, message);
+        public static void Warning(object message) => Log(ELogLevel.Warning, message);
 
         /// <summary>
-        ///     打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -67,10 +69,11 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_DEBUG_AND_ABOVE_LOG")]
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
-        public static void Warning(string message) => LogHelper.Log(LogLevel.Warning, message);
+        public static void Warning(string message) => Log(ELogLevel.Warning, message);
 
+        
         /// <summary>
-        ///     打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -79,10 +82,10 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
         [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
-        public static void Error(object message) => LogHelper.Log(LogLevel.Error, message);
+        public static void Error(object message) => Log(ELogLevel.Error, message);
 
         /// <summary>
-        ///     打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
+        /// 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -91,10 +94,11 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_INFO_AND_ABOVE_LOG")]
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
         [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
-        public static void Error(string message) => LogHelper.Log(LogLevel.Error, message);
+        public static void Error(string message) => Log(ELogLevel.Error, message);
 
+        
         /// <summary>
-        ///     打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -104,10 +108,10 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
         [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
         [Conditional("ENABLE_FATAL_AND_ABOVE_LOG")]
-        public static void Fatal(object message) => LogHelper.Log(LogLevel.Fatal, message);
+        public static void Fatal(object message) => Log(ELogLevel.Fatal, message);
 
         /// <summary>
-        ///     打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
+        /// 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架。
         /// </summary>
         /// <param name="message">日志内容。</param>
         [Conditional("ENABLE_LOG")]
@@ -117,6 +121,29 @@ namespace FuFramework.Core.Runtime
         [Conditional("ENABLE_WARNING_AND_ABOVE_LOG")]
         [Conditional("ENABLE_ERROR_AND_ABOVE_LOG")]
         [Conditional("ENABLE_FATAL_AND_ABOVE_LOG")]
-        public static void Fatal(string message) => LogHelper.Log(LogLevel.Fatal, message);
+        public static void Fatal(string message) => Log(ELogLevel.Fatal, message);
+        
+        
+        /// <summary>
+        /// 记录日志。
+        /// </summary>
+        /// <param name="level">日志等级。</param>
+        /// <param name="message">日志内容。</param>
+        public static void Log(ELogLevel level, object message)
+        {
+            var time = $"[Unity]:[{DateTime.Now:HH:mm:ss.fff}]:";
+
+            switch (level)
+            {
+                // @formatter:off
+                case ELogLevel.Debug:   UnityEngine.Debug.Log($"{time}{message}");        break;
+                case ELogLevel.Info:    UnityEngine.Debug.Log($"{time}{message}");        break;
+                case ELogLevel.Warning: UnityEngine.Debug.LogWarning($"{time}{message}"); break;
+                case ELogLevel.Error:   UnityEngine.Debug.LogError($"{time}{message}");   break;
+                case ELogLevel.Fatal:   UnityEngine.Debug.LogError($"{time}{message}");   break;
+                default:               throw new FuException($"{time}{message}");
+                // @formatter:on
+            }
+        }
     }
 }
