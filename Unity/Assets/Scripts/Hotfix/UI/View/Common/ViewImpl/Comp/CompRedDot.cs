10a30c09720e717fc8ccaf685ddbf09e7f02a2b5
MrZhifu <1206580585@qq.com>
2025-11-13 02:06:27 +0800

    fix：通用红点组件

--- Unity/Assets/Scripts/Hotfix/UI/View/Common/ViewImpl/Comp/CompRedDot.cs
+++ Unity/Assets/Scripts/Hotfix/UI/View/Common/ViewImpl/Comp/CompRedDot.cs
@@ -22,6 +22,11 @@ namespace Hotfix.UI
         private string m_RedDotKey;
 
         /// <summary>
+        /// 红点显示模式
+        /// </summary>
+        private RedDotDisplayMode m_RedDotDisplayMode = RedDotDisplayMode.DotOnly;
+
+        /// <summary>
         /// 初始化
         /// </summary>
         private void OnInit() { }
@@ -30,13 +35,13 @@ namespace Hotfix.UI
         /// 注册相关逻辑事件
         /// </summary>
         private void InitEvent() { }
-        
+
         /// <summary>
         /// 销毁。
         /// 注意：UI事件，业务逻辑事件，计时器会自动从所属的View中移除，无需在这里手动移除。
         /// </summary>
         private void OnDispose() { }
-        
+
         /// <summary>
         /// 设置所属界面
         /// </summary>
@@ -51,6 +56,15 @@ namespace Hotfix.UI
         {
             if (string.IsNullOrEmpty(redDotKey)) return;
             m_RedDotKey = redDotKey;
+        }
+
+        /// <summary>
+        /// 设置红点显示模式
+        /// </summary>
+        /// <param name="redDotDisplayMode"></param>
+        public void SetRedDotDisplayMode(RedDotDisplayMode redDotDisplayMode)
+        {
+            m_RedDotDisplayMode = redDotDisplayMode;
         }
     }
 }
