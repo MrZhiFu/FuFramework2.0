983fc492583013fface0f57a361b0b87f01d4f9d
MrZhifu <1206580585@qq.com>
2026-03-25 16:22:52 +0800

    feat： 自定义导出目标为cs-dotnet-bin

---
+++ Config/luban/src/Luban.CSharp/CodeTarget/CsharpDotNetBinCodeTarget.cs
@@ -0,0 +1,18 @@
+﻿using Luban.CodeTarget;
+using Luban.CSharp.TemplateExtensions;
+using Scriban;
+
+namespace Luban.CSharp.CodeTarget;
+
+/// <summary>
+/// 自定义导出目标为cs-dotnet-bin(服务器用)
+/// </summary>
+[CodeTarget("cs-dotnet-bin")]
+public class CsharpDotNetBinCodeTarget : CsharpCodeTargetBase
+{
+    protected override void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        base.OnCreateTemplateContext(ctx);
+        ctx.PushGlobal(new CsharpBinTemplateExtension());
+    }
+}
