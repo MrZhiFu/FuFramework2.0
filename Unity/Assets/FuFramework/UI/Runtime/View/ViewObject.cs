fa14025118815d0e36344f86394816762ea46d62
MrZhifu <1206580585@qq.com>
2026-04-08 11:43:43 +0800

    refactor：UI模块目录调整

---
+++ Unity/Assets/FuFramework/UI/Runtime/View/ViewObject.cs
@@ -0,0 +1,42 @@
+﻿using FuFramework.Core.Runtime;
+using FuFramework.ObjectPool.Runtime;
+
+// ReSharper disable once CheckNamespace
+namespace FuFramework.UI.Runtime
+{
+    /// <summary>
+    /// 界面实例对象。
+    /// 职责：创建和释放界面实例对象。
+    /// 核心功能:
+    /// 1. 创建界面实例对象。
+    /// 2. 释放界面实例对象。
+    /// </summary>
+    public sealed class ViewObject : ObjectBase
+    {
+        /// <summary>
+        /// 创建界面实例对象。
+        /// </summary>
+        /// <param name="uiName"></param>
+        /// <param name="viewBase"></param>
+        /// <returns></returns>
+        public static ViewObject Create(string uiName, ViewBase viewBase)
+        {
+            var uiInstanceObject = ReferencePool.Runtime.ReferencePool.Acquire<ViewObject>();
+            uiInstanceObject.Initialize(uiName, viewBase);
+            return uiInstanceObject;
+        }
+
+        /// <summary>
+        /// 释放界面实例对象。
+        /// </summary>
+        /// <param name="isShutdown"></param>
+        protected override void Release(bool isShutdown)
+        {
+            if (Target is not ViewBase viewBase)
+                throw new FuException("[UIInstanceObject] 需要释放的目标对象不是界面基类ViewBase");
+
+            viewBase.UIView?.Dispose();
+            viewBase._OnDispose();
+        }
+    }
+}
