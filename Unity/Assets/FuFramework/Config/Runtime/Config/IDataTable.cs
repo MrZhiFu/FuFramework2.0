43ac5c74365e7d0906c82bb0bc7a4294103d996e
MrZhifu <1206580585@qq.com>
2026-04-12 16:09:07 +0800

    docs：配置表模块Readme文档修改

--- Unity/Assets/FuFramework/Config/Runtime/Config/IDataTable.cs
+++ Unity/Assets/FuFramework/Config/Runtime/Config/IDataTable.cs
@@ -7,7 +7,7 @@ namespace FuFramework.Config.Runtime
 {
     /// <summary>
     /// 数据表基础接口.
-    /// 功能：定义数据表的异步加载与数据行数量接口。
+    /// 功能：定义数据表的异步加载与数据表的行数(对象)接口。
     /// </summary>
     public interface IDataTable
     {
