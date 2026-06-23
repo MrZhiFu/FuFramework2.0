4b28882538bd09c71a85807796a9be0c1390fb79
MrZhifu <1206580585@qq.com>
2026-06-23 22:04:03 +0800

    chore：协议文件单独放在proto文件夹中

--- Protobuf/Proto2CsExport_Server.bat
+++ Protobuf/Proto2CsExport_Server.bat
@@ -1,4 +1,4 @@
 cd /d ../Tools/ProtoExport\bin\Debug\net8.0
-call dotnet ProtoExport.dll --mode server --inputPath ./../../../../../Protobuf --outputPath ./../../../../../Server/FuFramework.Proto/Proto --namespaceName FuFramework.Proto.Proto --isGenerateErrorCode true
+call dotnet ProtoExport.dll --mode server --inputPath ./../../../../../Protobuf/Proto --outputPath ./../../../../../Server/FuFramework.Proto/Proto --namespaceName FuFramework.Proto.Proto --isGenerateErrorCode true
 
 pause
--- Protobuf/Proto2CsExport_Server.sh
+++ Protobuf/Proto2CsExport_Server.sh
@@ -4,7 +4,7 @@
 cd -P ../Tools/ProtoExport/bin/Debug/net8.0
 
 # 启动应用程序，并将其放入后台执行
-dotnet ProtoExport.dll --mode server --inputPath ./../../../../../Protobuf --outputPath ./../../../../../Server/FuFramework.Proto/Proto --namespaceName FuFramework.Proto.Proto --isGenerateErrorCode true &
+dotnet ProtoExport.dll --mode server --inputPath ./../../../../../Protobuf/Proto --outputPath ./../../../../../Server/FuFramework.Proto/Proto --namespaceName FuFramework.Proto.Proto --isGenerateErrorCode true &
 
 # 输出提示信息
 echo "Press any key to continue . . ."
