f1caf7247ab25b3b90ba8b811b592c279853a33e
MrZhifu <1206580585@qq.com>
2026-07-13 22:40:29 +0800

    refactor: HotfixLauncher 委托挂接目标从 Launcher 迁移到 GameDriven.Instance

--- Unity/Assets/Scripts/Hotfix/HotfixLauncher.cs
+++ Unity/Assets/Scripts/Hotfix/HotfixLauncher.cs
@@ -79,12 +79,12 @@ namespace Hotfix
             ModuleManager.RegisterModule<WebModule>();
             ModuleManager.RegisterModule<NetworkModule>();
 
-            // 将 ModuleManager 的生命周期方法挂接到 AOT 侧 Launcher 委托（ModuleManager 已随 Task 15 下沉 Hotfix）
-            FuFramework.Launcher.Runtime.Launcher.OnUpdate       = ModuleManager.Update;
-            FuFramework.Launcher.Runtime.Launcher.OnLateUpdate   = ModuleManager.LateUpdate;
-            FuFramework.Launcher.Runtime.Launcher.OnFixedUpdate  = ModuleManager.FixedUpdate;
-            FuFramework.Launcher.Runtime.Launcher.DisposeModules = ModuleManager.Dispose;
-            FuFramework.Launcher.Runtime.Launcher.ReInitModules  = ModuleManager.ReInit;
+            // 将 ModuleManager 的生命周期方法挂接到 GameDriven 委托
+            GameDriven.Instance.OnUpdate       = ModuleManager.Update;
+            GameDriven.Instance.OnLateUpdate   = ModuleManager.LateUpdate;
+            GameDriven.Instance.OnFixedUpdate  = ModuleManager.FixedUpdate;
+            GameDriven.Instance.DisposeModules = ModuleManager.Dispose;
+            GameDriven.Instance.ReInitModules  = ModuleManager.ReInit;
 
             // 加载配置表
             bootstrapView.SetTip("LoadConfig...");
