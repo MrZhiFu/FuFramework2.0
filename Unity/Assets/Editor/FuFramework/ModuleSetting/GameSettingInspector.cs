02c52a8df288f1ae76672eebe1a51b508629aeed
MrZhifu <1206580585@qq.com>
2026-07-22 22:51:40 +0800

    refactor: GameSettingInspector 去掉速度倍率按钮，只保留进度条

--- Unity/Assets/Editor/FuFramework/ModuleSetting/GameSettingInspector.cs
+++ Unity/Assets/Editor/FuFramework/ModuleSetting/GameSettingInspector.cs
@@ -11,12 +11,6 @@ namespace AOT.Framework.ModuleSetting.Editor
     [CustomEditor(typeof(Runtime.GameSetting))]
     internal sealed class GameSettingInspector : FuFrameworkInspector
     {
-        /// 游戏速度数组
-        private static readonly float[] GameSpeed = { 0f, 0.01f, 0.1f, 0.25f, 0.5f, 1f, 1.5f, 2f, 4f, 8f };
-
-        /// 游戏速度显示名称数组
-        private static readonly string[] GameSpeedForDisplay = { "0x", "0.01x", "0.1x", "0.25x", "0.5x", "1x", "1.5x", "2x", "4x", "8x" };
-
         private SerializedProperty m_FrameRate;       // 帧率
         private SerializedProperty m_GameSpeed;       // 游戏速度
         private SerializedProperty m_RunInBackground; // 是否后台运行
@@ -73,24 +67,14 @@ namespace AOT.Framework.ModuleSetting.Editor
             }
 
             // 游戏速度
-            EditorGUILayout.BeginVertical("box");
+            var gameSpeed = EditorGUILayout.Slider("游戏速度设置：", m_GameSpeed.floatValue, 0f, 8f);
+            if (!Mathf.Approximately(gameSpeed, m_GameSpeed.floatValue))
             {
-                var gameSpeed         = EditorGUILayout.Slider("游戏速度设置：", m_GameSpeed.floatValue, 0f, 8f);
-                var selectedGameSpeed = GUILayout.SelectionGrid(GetSelectedGameSpeed(gameSpeed), GameSpeedForDisplay, 5);
-                if (selectedGameSpeed >= 0)
-                {
-                    gameSpeed = GetGameSpeed(selectedGameSpeed);
-                }
-
-                if (!Mathf.Approximately(gameSpeed, m_GameSpeed.floatValue))
-                {
-                    if (EditorApplication.isPlaying)
-                        gameSetting.GameSpeed = gameSpeed;
-                    else
-                        m_GameSpeed.floatValue = gameSpeed;
-                }
+                if (EditorApplication.isPlaying)
+                    gameSetting.GameSpeed = gameSpeed;
+                else
+                    m_GameSpeed.floatValue = gameSpeed;
             }
-            EditorGUILayout.EndVertical();
 
             // 设置是否后台运行
             var runInBackground = EditorGUILayout.Toggle("是否可在后台运行", m_RunInBackground.boolValue);
@@ -139,33 +123,6 @@ namespace AOT.Framework.ModuleSetting.Editor
             EditorGUILayout.PropertyField(m_EncryptKey);
 
             serializedObject.ApplyModifiedProperties();
-        }
-
-        /// <summary>
-        /// 获取游戏速度
-        /// </summary>
-        /// <param name="selectedGameSpeed"></param>
-        /// <returns></returns>
-        private static float GetGameSpeed(int selectedGameSpeed)
-        {
-            if (selectedGameSpeed < 0) return GameSpeed[0];
-            return selectedGameSpeed >= GameSpeed.Length ? GameSpeed[^1] : GameSpeed[selectedGameSpeed];
-        }
-
-        /// <summary>
-        /// 获取当前游戏速度的索引
-        /// </summary>
-        /// <param name="gameSpeed"></param>
-        /// <returns></returns>
-        private static int GetSelectedGameSpeed(float gameSpeed)
-        {
-            for (var i = 0; i < GameSpeed.Length; i++)
-            {
-                if (Mathf.Approximately(gameSpeed, GameSpeed[i]))
-                    return i;
-            }
-
-            return -1;
         }
     }
 }
